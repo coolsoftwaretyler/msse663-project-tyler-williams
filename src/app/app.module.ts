517b5baab383df8fe4294cfa68f7fa9cec3a2511
Tyler Williams <tyler@coolsoftware.dev>
2021-06-06 10:51:44 -0600

    interpret input from textarea

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +16,8 @@ import { ConsoleComponent } from './console/console.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/text-editor/text-editor.component.ts
+++ src/app/text-editor/text-editor.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+import { interpret, parse } from '../lisp';
 
 @Component({
   selector: 'app-text-editor',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./text-editor.component.scss']
 })
 export class TextEditorComponent implements OnInit {
+  public editorForm: FormGroup
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    this.editorForm = this.formBuilder.group({
+      source: '',
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    const input = this.editorForm.controls.source.value;
+    const parsedInput = parse(`"${input}"`);
+    const interpretedInput = interpret(parsedInput, undefined);
+    console.log(interpretedInput);
   }
 
 }
