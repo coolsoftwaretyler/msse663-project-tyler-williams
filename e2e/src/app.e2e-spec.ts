71a9a10ee9745f3dade7ca33596eb9b731a0ea6f
Tyler Williams <tyler@coolsoftware.dev>
2021-06-23 12:33:02 -0600

    finish end to end tests for all existing functionality

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,8 +1,9 @@
-import { browser, logging, ExpectedConditions } from 'protractor';
+import { browser, logging, ExpectedConditions, element, by } from 'protractor';
 import { AppPage } from './app.po';
 
 describe('Lisp REPL App', () => {
   let page: AppPage;
+  let programId: string;
 
   beforeEach(() => {
     page = new AppPage();
@@ -17,14 +18,53 @@ describe('Lisp REPL App', () => {
     });
   });
 
-  it('should be able to navigate to the edit page', async () => {
+  it('should be able to create a new program', async () => {
     await page.navigateTo();
     await page.clickCreateLink();
 
-    browser.wait(ExpectedConditions.urlContains('programs/new'), 5000).then(function(result) {
+    await browser.wait(ExpectedConditions.urlContains('programs/new'), 5000);
+    await page.writeShortLispProgram('"abc"');
+    await page.clickTextEditorSubmit();
+    await browser.wait(ExpectedConditions.urlContains('/programs/'), 5000);
+    const currentUrl = await browser.getCurrentUrl();
+    const currentUrlDelimited = currentUrl.split('/')
+    programId = currentUrlDelimited[currentUrlDelimited.length - 1]
+    const result = await page.getConsoleOutputText();
+    expect(result).toEqual("abc");
+  });
+
+  it('should be able to navigate to the edit page', async () => {
+    await page.navigateTo();
+    await page.clickEditLink();
+
+    browser.wait(ExpectedConditions.urlContains('programs/search'), 5000).then(function(result) {
         expect(result).toEqual(true);
     });
   });
+
+  it ('should be able to search for a program by ID', async () => {
+    await page.navigateTo();
+    await page.clickEditLink();
+    await browser.wait(ExpectedConditions.urlContains('programs/search'), 5000)
+    await page.searchforLispProgram(programId);
+    await browser.wait(ExpectedConditions.urlContains('/programs/' + programId), 5000);
+    const result = await page.getConsoleOutputText();
+    expect(result).toEqual("abc");
+  })
+
+  it ('should be able to directly visit a program edit page', async () => {
+    await page.navigateToEditor(programId);
+    const result = await page.getConsoleOutputText();
+    expect(result).toEqual("abc");
+  })
+
+  it('should be able to edit a program', async () => {
+    await page.navigateToEditor(programId);
+    await page.writeShortLispProgram('"def"');
+    await page.clickTextEditorSubmit();
+    const result = await page.getConsoleOutputText();
+    expect(result).toEqual("def");
+  })
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -12,4 +12,30 @@ export class AppPage {
   async clickEditLink(): Promise<unknown> {
     return element(by.css('[data-protractor="edit"]')).click();
   }
+
+  async writeShortLispProgram(program: string): Promise<unknown> {
+    return element(by.css('[data-protractor="textEditorTextArea"]')).clear()
+    .then(() => {
+      element(by.css('[data-protractor="textEditorTextArea"]')).sendKeys(program);
+    })
+  }
+
+  async clickTextEditorSubmit(): Promise<unknown> {
+    return element(by.css('[data-protractor="textEditorSubmit"]')).click();
+  }
+
+  async getConsoleOutputText(): Promise<unknown> {
+    return element(by.css('[data-protractor="consoleOutput"]')).getText();
+  }
+
+  async searchforLispProgram(programId: string): Promise<unknown> {
+    return element(by.css('[data-protractor="programId"]')).sendKeys(programId)
+    .then(function() {
+      element(by.css('[data-protractor="searchSubmit"]')).click();
+    })
+  }
+
+  async navigateToEditor(programId: String): Promise<unknown> {
+    return browser.get(browser.baseUrl + '/programs/' + programId);
+  }
 }
