895802531f59744c20364502e472058f8a7bbd05
Tyler Williams <tyler@coolsoftware.dev>
2021-06-23 21:03:18 -0600

    add concurrently and move mongo credentials to env variable

--- src/backend/config/db.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  url: "mongodb+srv://msse663:b2Xr52cm5L2KuzV@cluster0.73dsl.mongodb.net/Cluster0?retryWrites=true&w=majority"
-};
--- src/backend/models/index.js
+++ src/backend/models/index.js
@@ -1,11 +1,11 @@
-const dbConfig = require("../config/db.config.js");
+require('dotenv').config();
 
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-
+const url =  `mongodb+srv://msse663:${process.env.MONGO_PASSWORD}@cluster0.73dsl.mongodb.net/Cluster0?retryWrites=true&w=majority`
 const db = {};
 db.mongoose = mongoose;
-db.url = dbConfig.url;
+db.url = url;
 db.programs = require("./program.model.js")(mongoose);
 
 module.exports = db;
