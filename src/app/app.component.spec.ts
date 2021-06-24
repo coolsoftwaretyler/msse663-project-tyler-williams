ab45ad8eb8a0f7017ef6ed2e25bf518de985f300
Tyler Williams <tyler@coolsoftware.dev>
2021-06-23 21:39:06 -0600

    fix component unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('msse663-project-tyler-williams');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('msse663-project-tyler-williams app is running!');
-  });
 });
--- src/app/text-editor/text-editor.component.spec.ts
+++ src/app/text-editor/text-editor.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 import { TextEditorComponent } from './text-editor.component';
 
@@ -8,7 +10,8 @@ describe('TextEditorComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TextEditorComponent ]
+      imports: [FormsModule, ReactiveFormsModule, HttpClientModule],
+      declarations: [TextEditorComponent ]
     })
     .compileComponents();
   });
